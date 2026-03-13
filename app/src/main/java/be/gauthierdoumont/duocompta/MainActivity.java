f08376ae9d2966289a763d90a716e0cc01cfe65a
gauthierdoumont <gauthierdoumont@gmail.com>
2026-03-13 14:18:04 +0100

    Update MainActivity.java

--- app/src/main/java/be/gauthierdoumont/duocompta/MainActivity.java
+++ app/src/main/java/be/gauthierdoumont/duocompta/MainActivity.java
@@ -2,18 +2,22 @@ package be.gauthierdoumont.duocompta;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebSettings;
 import android.webkit.WebChromeClient;
 import android.webkit.PermissionRequest;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceError;
 import android.view.Window;
 import android.view.WindowManager;
 import android.graphics.Color;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "DuoCompta";
     private WebView webView;
 
     @Override
@@ -25,6 +29,9 @@ public class MainActivity extends Activity {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
+
+        // Activer le debug WebView
+        WebView.setWebContentsDebuggingEnabled(true);
 
         webView = new WebView(this);
         webView.setBackgroundColor(Color.parseColor("#0e0f14"));
@@ -39,18 +46,39 @@ public class MainActivity extends Activity {
         s.setAllowFileAccessFromFileURLs(true);
         s.setAllowUniversalAccessFromFileURLs(true);
         s.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        s.setCacheMode(WebSettings.LOAD_DEFAULT);
+        s.setCacheMode(WebSettings.LOAD_NO_CACHE);
         s.setLoadWithOverviewMode(true);
         s.setUseWideViewPort(true);
         s.setBuiltInZoomControls(false);
         s.setDisplayZoomControls(false);
         s.setMediaPlaybackRequiresUserGesture(false);
+        s.setBlockNetworkImage(false);
+        s.setLoadsImagesAutomatically(true);
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                view.loadUrl(request.getUrl().toString());
-                return true;
+            public void onPageFinished(WebView view, String url) {
+                Log.d(TAG, "Page chargee: " + url);
+                // Forcer le fond sombre apres chargement
+                view.setBackgroundColor(Color.parseColor("#0e0f14"));
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request,
+                                        WebResourceError error) {
+                Log.e(TAG, "Erreur WebView: " + error.getDescription()
+                    + " url: " + request.getUrl());
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view,
+                                                    WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                if (url.startsWith("file://") || url.startsWith("http")) {
+                    view.loadUrl(url);
+                    return true;
+                }
+                return false;
             }
         });
 
@@ -59,9 +87,24 @@ public class MainActivity extends Activity {
             public void onPermissionRequest(PermissionRequest request) {
                 request.grant(request.getResources());
             }
+
+            @Override
+            public boolean onConsoleMessage(
+                    android.webkit.ConsoleMessage msg) {
+                Log.d(TAG, "JS Console: " + msg.message()
+                    + " (" + msg.sourceId() + ":" + msg.lineNumber() + ")");
+                return true;
+            }
         });
 
-        webView.loadUrl("file:///android_asset/index.html");
+        // Charger avec un petit delai pour laisser la WebView s'initialiser
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "Chargement index.html...");
+                webView.loadUrl("file:///android_asset/index.html");
+            }
+        }, 300);
     }
 
     @Override
@@ -83,5 +126,13 @@ public class MainActivity extends Activity {
     protected void onResume() {
         super.onResume();
         webView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.destroy();
+        }
+        super.onDestroy();
     }
 }
